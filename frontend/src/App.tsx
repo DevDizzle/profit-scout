ddbbde158c191fadde5f935a28080c0f556bed94
DevDizzle <eraphaelparra@gmail.com>
2025-03-31 14:40:37 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,9 @@ import ChatUI from "./components/ChatUI";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+    <div className="min-h-screen flex flex-col bg-gradient-to-b from-[#1A1A1A] to-[#252525] text-white">
       <Header />
-      <main className="flex flex-1 items-center justify-center p-4">
+      <main className="flex flex-1 items-center justify-center px-4 py-8">
         <ChatUI />
       </main>
     </div>
