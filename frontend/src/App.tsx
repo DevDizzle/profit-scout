975fd7b886fa09935076f716c34333551aba154e
DevDizzle <eraphaelparra@gmail.com>
2025-03-31 13:16:56 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import ChatUI from "./components/ChatUI";
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
-      <ChatUI />
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 bg-gray-100 p-4">
+          <ChatUI />
+        </main>
+      </div>
     </div>
   );
 }
